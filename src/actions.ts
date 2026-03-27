a9fc0974b563712e008a579f88d9c9c6c00f95c8
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2026-03-27 16:48:10 +1100

    rework action logging, add note field to insert bookmark action

--- src/actions.ts
+++ src/actions.ts
@@ -3,7 +3,7 @@ import type { CompanionActionDefinition } from '@companion-module/base'
 
 export enum ActionId {
 	NewFile = 'new_file',
-	CloseFile = 'close_file',
+	//  CloseFile = 'close_file',
 	StartRecording = 'start_recording',
 	StopRecording = 'stop_recording',
 	PauseRecording = 'pause_recording',
@@ -15,49 +15,50 @@ export function UpdateActions(self: ModuleInstance): void {
 		[ActionId.NewFile]: {
 			name: 'File: New',
 			options: [],
-			callback: async (_event) => {
-				self.log('info', 'File: New')
+			callback: async (event) => {
+				self.log('info', `${event.actionId}:${event.id}`)
 				await self.client.newFile()
-			},
-		},
-		[ActionId.CloseFile]: {
-			name: 'File: Close',
-			options: [],
-			callback: async (_event) => {
-				self.log('info', 'File: Close')
-				await self.client.closeFile()
 			},
 		},
 		[ActionId.StartRecording]: {
 			name: 'Recording: Start',
 			options: [],
-			callback: async (_event) => {
-				self.log('info', 'Record: Stop')
+			callback: async (event) => {
+				self.log('info', `${event.actionId}:${event.id}`)
 				await self.client.startRecording()
 			},
 		},
 		[ActionId.StopRecording]: {
-			name: 'Recording: Stop',
+			name: 'Recording: Stop & Close File',
 			options: [],
 			callback: async (_event) => {
-				self.log('info', 'Record: Stop')
+				self.log('info', 'Record: Stop & Close File')
 				await self.client.stopRecording()
 			},
 		},
 		[ActionId.PauseRecording]: {
 			name: 'Recording: Pause',
 			options: [],
-			callback: async (_event) => {
-				self.log('info', 'Record: Pause')
+			callback: async (event) => {
+				self.log('info', `${event.actionId}:${event.id}`)
 				await self.client.pauseRecording()
 			},
 		},
 		[ActionId.InsertBookmark]: {
 			name: 'Bookmark: Insert',
-			options: [],
-			callback: async (_event) => {
-				self.log('info', 'Bookmark: Insert')
-				await self.client.insertBookmark()
+			options: [
+				{
+					type: 'textinput',
+					id: 'note',
+					label: 'Note',
+					useVariables: { local: true },
+					multiline: true,
+					default: '',
+				},
+			],
+			callback: async (event) => {
+				self.log('info', `${event.actionId}:${event.id} - ${event.options.note}`)
+				await self.client.insertBookmark(event.options.note?.toString())
 			},
 		},
 	}
